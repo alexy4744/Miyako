07b1e8e6996c50508ae39e00a448996b6b85368e
alexy4744 <alexy4744@hstat.org>
2018-07-13 15:08:43 -0400

    webscraper is now decent enough

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,25 +1,23 @@
-// Originally from https://stdlib.com/@nemo/lib/scrape
 const request = require("request-promise-native");
 const cheerio = require("cheerio");
 const parseAll = require("html-metadata").parseAll;
 
-module.exports = async (url, selector, action) => {
-  const $ = await request({
-    url: "https://soundcloud.com/catvlyst/tell-me",
+module.exports = (url, selector, fn, args = []) => new Promise(async (resolve, reject) => {
+  const opts = {
+    url: "https://soundcloud.com/mrsuicidesheep/no-sleep-a-chill-mix",
     headers: {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
     },
     transform: body => cheerio.load(body)
-  }).catch(e => {
-    throw new Error(e);
-  });
-
-  const metadata = await parseAll($).catch(e => {
-    throw new Error(e);
-  });
-  console.log(metadata)
-  return {
-    $: $,
-    metadata: metadata
   };
-};
+
+  const $ = await request(opts).catch(e => reject(e));
+  const metadata = await parseAll($).then(() => metadata.url = url).catch(e => reject(e));
+
+  if (!selector && !fn && args.length < 1) resolve(metadata); // eslint-disable-line
+  else resolve({ // eslint-disable-line
+    url: url,
+    meta: metadata,
+    scraped: ($(selector).length ? $(selector).toArray() : [$(selector)]).map(el => $(el)[fn](...args))
+  });
+});
