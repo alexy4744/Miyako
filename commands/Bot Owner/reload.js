2ad46c40256019c0bfd552e7653b93d26b60664d
alexy4744 <alexy4744@gmail.com>
2018-11-12 22:44:55 -0500

    update reload.js

--- commands/Bot Owner/reload.js
+++ commands/Bot Owner/reload.js
@@ -8,7 +8,7 @@ module.exports = class extends Command {
       botOwnerOnly: true,
       nsfw: false,
       cooldown: 5,
-      description: () => `Reload commands, events, inhibitors, monitors, tasks`,
+      description: () => `Reload commands, events, inhibitors, finalizers, monitors, tasks`,
       aliases: [],
       userPermissions: [],
       botPermissions: [],
@@ -17,13 +17,16 @@ module.exports = class extends Command {
   }
 
   run(msg, args) {
-    if (!args[0]) return msg.fail("You must supply a command/inhibitor/utility to reload!");
+    if (!args[0]) return msg.fail("You must supply a command/event/inhibitor/finalizer/monitor/task to reload!");
 
     const thingToReload = args[0];
 
     if (this.client.commands[thingToReload] || this.client.aliases[thingToReload]) this.client.commands[thingToReload].reload();
+    else if (this.client.events[thingToReload]) this.client.events[thingToReload].reload();
     else if (this.client.inhibitors[thingToReload]) this.client.inhibitors[thingToReload].reload();
     else if (this.client.finalizers[thingToReload]) this.client.finalizers[thingToReload].reload();
+    else if (this.client.monitors[thingToReload]) this.client.monitors[thingToReload].reload();
+    else if (this.client.tasks[thingToReload]) this.client.tasks[thingToReload].reload();
 
     return msg.success(`"${thingToReload}" has been succesfully reloaded!`);
   }
